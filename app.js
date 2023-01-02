efb3f1101543c5432948709767a7f6b14a3269de
Leandro Assis <leandrou9@gmail.com>
2023-01-02 10:15:50 -0300

    Configurando Express e Postman Implementando métodos http para get e post

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const app = express();
 
 // Iniciando um servidor
 
-app.get('/', (req, res) => {    //definindo rota
-    //200 para ok
-    res.status(200).send('Hola señores ((del servidor');
+app.get('/', (req, res) => {    //definindo rota (método http request)
+    //200 para ok (default) 404 para not found
+    res
+        .status(200)
+        .json({mensagem: 'Hola señores ((del servidor', app: 'Natures'});
 });
+
+app.post('/', (req, res) => {
+    res.send('Podes postar neste endpoint');
+})
 
 const porta = 8000;
 app.listen(porta, () => {
