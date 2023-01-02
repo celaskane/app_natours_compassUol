462a2245276111dfebc6a498fab2eff9ce046ac4
Leandro Assis <leandrou9@gmail.com>
2023-01-02 11:05:52 -0300

    Iniciando API implementando método GET (rout handler) utilizando API REST

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
 // Iniciando um servidor
 
-app.get('/', (req, res) => {    //definindo rota (método http request)
+/* app.get('/', (req, res) => {    //definindo rota (método http request)
     //200 para ok (default) 404 para not found
     res
         .status(200)
@@ -13,9 +13,24 @@ app.get('/', (req, res) => {    //definindo rota (método http request)
 
 app.post('/', (req, res) => {
     res.send('Podes postar neste endpoint');
-})
+}) */
 
-const porta = 8000;
+
+//Iniciando API: GET (route handler)
+// realizar a leitura dos dados primeiro
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+
+const porta = 8000;         //inicializando servidor
 app.listen(porta, () => {
     console.log(`App funcionando na porta ${porta}`);
 });
