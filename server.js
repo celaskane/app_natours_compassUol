351c7bbecf6d46441cfcfdd50671204d3b5c05bb
Leandro Assis <leandrou9@gmail.com>
2023-01-05 13:01:57 -0300

    Criando esquema e modelo para o banco de dados

--- server.js
+++ server.js
@@ -20,6 +20,23 @@ mongoose
     console.log('Conexão com Banco de Dados OK');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour precisa de um nome'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour precisa de um preco'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 //NODE_ENV=development nodemon server.js
 /* console.log(app.get('env')); */ //consultar ambiente
 /* console.log(process.env); */ //consultar variáveis de ambiente
