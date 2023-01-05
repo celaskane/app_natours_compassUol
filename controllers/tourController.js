b4e610a9dbeb867a76d4fdf487c3ad45a8f24b68
Leandro Assis <leandrou9@gmail.com>
2023-01-05 14:37:19 -0300

    Refatorando controllers para adequar à arquitetura MCV

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,11 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-//Iniciando route handler (realizar a leitura dos dados primeiro)
+/* //Iniciando route handler (realizar a leitura dos dados primeiro)
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+); */
 
 //middleware functions
-//valida ID
-exports.checkID = (req, res, next, val) => {
-  console.log(`ID da tour: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'ID Inválido',
-    });
-  }
-  next();
-};
 
 //valida nome ou preço
 exports.checkBody = (req, res, next) => {
@@ -35,24 +24,18 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
+    /* results: tours.length,
     data: {
       tours,
-    },
+    }, */
   });
 };
 
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; //convertendo string para numero
-  const tour = tours.find((el) => el.id === id);
-  //if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'ID Inválido',
-    });
-  }
+
+  /* const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -60,41 +43,22 @@ exports.getTour = (req, res) => {
     data: {
       tour,
     },
-  });
+  }); */
 };
 
 // POST (se necessário, reiniciar o nodemon)
 exports.createTour = (req, res) => {
-  //middleware
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        //201 significa criado
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    //201 significa criado
+    status: 'success',
+    /* data: {
+      tour: newTour,
+    }, */
+  });
 };
 
 // PATCH (update de propriedades específicas)
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'ID Inválido',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
