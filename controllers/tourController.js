f040ceecf12efcd44a0bae857c9bdc09b401272a
Leandro Assis <leandrou9@gmail.com>
2023-01-05 16:11:25 -0300

    Método http PATCH implementado com async/await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -63,13 +63,25 @@ exports.createTour = async (req, res) => {
 };
 
 // PATCH (update de propriedades específicas)
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Tour atualizado...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //return documento atualizado
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // DELETE
