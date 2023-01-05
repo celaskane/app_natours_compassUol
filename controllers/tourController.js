703b9e9b6a1cc6cccc9ce03a1b6196211fc4261c
Leandro Assis <leandrou9@gmail.com>
2023-01-05 15:36:02 -0300

    Método http Get implementando com async/await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,16 +6,23 @@ const tours = JSON.parse(
 ); */
 
 // GET
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    /* results: tours.length,
-    data: {
-      tours,
-    }, */
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
