0af2bb42e0480ffa7009ff287e91d1a6fa636bcf
Leandro Assis <leandrou9@gmail.com>
2023-01-05 15:48:10 -0300

    Método http GET (por ID) implementado com async/await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,19 +25,22 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //convertendo string para numero
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  /* const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tour,
-    },
-  }); */
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // POST
