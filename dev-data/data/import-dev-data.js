0c7a33f2aef55f913a117fb3e2c0005dccd23731
Leandro Assis <leandrou9@gmail.com>
2023-01-09 17:46:48 -0300

    Implementando import e delete Import e Delete dos dados contidos no arquivo .json Envio e exclusão dos dados para o banco de dados

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Conexão com o banco de dados OK'));
+
+// Lendo o arquivo json
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Importando dados do json no banco de dados
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Dados carregados com sucesso!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Apagando os dados
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Dados apagados com sucesso!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
