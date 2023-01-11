bdfa7a5f14a251f0de0007fb031801e2255be9ce
Leandro Assis <leandrou9@gmail.com>
2023-01-11 11:41:29 -0300

    Filtragens adicionadas ao método GET sort, field, page e limit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,17 +8,39 @@ const tours = JSON.parse(
 // GET
 exports.getAllTours = async (req, res) => {
   try {
-    // Filtrando resultados
-    let queryStr = JSON.stringify(req.query);
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     let query = Tour.find(JSON.parse(queryStr));
+
     if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').jon(' ');
+      const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort('-_id');
     }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     const tours = await query;
 
     res.status(200).json({
@@ -31,7 +53,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
