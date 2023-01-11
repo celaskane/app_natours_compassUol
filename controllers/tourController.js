abcdf2d39bbccd86eb082aa12c347b1656bc5c80
Leandro Assis <leandrou9@gmail.com>
2023-01-11 15:51:14 -0300

    Desenvolvendo Aggregation Pipeline Implementando match e group

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -116,3 +116,40 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// Aggregation Pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$max' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
