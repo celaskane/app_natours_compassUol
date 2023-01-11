d3665e3575b50bea64d9ef38ff12af03d0151055
Leandro Assis <leandrou9@gmail.com>
2023-01-11 16:36:02 -0300

    Criando novo router Router referente ao novo aggregation pipeline retornara as tours mensais

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,9 @@ router
   .route('/top-5-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+// Aggregation Pipeline
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/') //tourRouter já está dentro de /api/v1/tours
