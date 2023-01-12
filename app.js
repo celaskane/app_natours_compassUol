c2a2fab0f0825151fb77f2f4853bd87f64d40216
Leandro Assis <leandrou9@gmail.com>
2023-01-12 10:31:05 -0300

    Implementando middleware para tratamento global de erros

--- app.js
+++ app.js
@@ -25,9 +25,26 @@ app.use('/api/v1/users', userRouter);
 
 // Rotas não existentes (precisa estar depois dos routers)
 app.all('*', (req, res, next) => {
-  res.status(404).json({
+  /* res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server`,
+  }); */
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Middleware para Tratamento Global de Erros
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
