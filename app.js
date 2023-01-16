9db209e0242076a7129bcdf83d5f6128a65a2191
Leandro Assis <leandrou9@gmail.com>
2023-01-16 10:19:43 -0300

    Implementando pontos de segurança

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,12 +13,45 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//Secutirty HTTP headers
+app.use(helmet());
+
 //condicionando o uso do middleware morgan apenas para ambiente de desenvolvimento
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //middleware 3rd party
 }
-app.use(express.json()); //express.json (middleware)
+
+// Limitando requisições por API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' })); //express.json (middleware)
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
