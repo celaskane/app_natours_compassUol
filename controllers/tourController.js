3d28df5f13f9fc335e31aef5921f3666d406fce4
Leandro Assis <leandrou9@gmail.com>
2023-01-17 10:30:57 -0300

    Utilizando handler factory para o método DELETE Implementando para tour, review e user

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 /* //Iniciando route handler (realizar a leitura dos dados primeiro)
 const tours = JSON.parse(
@@ -82,19 +83,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 // DELETE
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(204).json({
-    //204 significa no content
-    status: 'success',
-    data: null,
-  });
-});
+exports.deleteTour = factory.deleteOne(Tour);
 
 // Aggregation Pipeline
 exports.getTourStats = catchAsync(async (req, res, next) => {
