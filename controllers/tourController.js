23dc6255f6f21fd0ece2b8ab9823f37b1f86dcff
Leandro Assis <leandrou9@gmail.com>
2023-01-17 11:33:21 -0300

    Utilizando handler factory nos métodos POST e PATCH

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,35 +52,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 // POST
-exports.createTour = catchAsync(async (req, res, next) => {
-  const newTour = await Tour.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-});
+exports.createTour = factory.createOne(Tour);
 
 // PATCH (update de propriedades específicas)
-exports.updateTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-    new: true, //return documento atualizado
-    runValidators: true,
-  });
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
+exports.updateTour = factory.updateOne(Tour);
 
 // DELETE
 exports.deleteTour = factory.deleteOne(Tour);
