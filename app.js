3e6ce1c570f12d40441bc9d31101edc04274c8f1
Leandro Assis <leandrou9@gmail.com>
2023-01-16 21:45:10 -0300

    Montando router para reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -62,6 +63,7 @@ app.use((req, res, next) => {
 //separando routers (mounting routers)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Rotas não existentes (precisa estar depois dos routers)
 app.all('*', (req, res, next) => {
