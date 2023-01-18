57c6670f4eb3e08654cefb0364b20305b730c8cc
Leandro Assis <leandrou9@gmail.com>
2023-01-18 10:56:43 -0300

    Prevenindo reviews duplicadas

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Prevenindo reviews duplicadas
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
