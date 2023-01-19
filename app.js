eada9766ce9754a10eadb7f3113f2733f2316ebb
Leandro Assis <leandrou9@gmail.com>
2023-01-19 09:08:24 -0300

    Criando objeto para passagem de dados ao Pug

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ app.use((req, res, next) => {
 
 //separando routers (mounting routers)
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Alejandro',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
