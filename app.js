2f3cebbd42f5dd62a320e647845643b8acaa2457
Leandro Assis <leandrou9@gmail.com>
2023-01-20 19:23:45 -0300

    Alterando helmet para utilização do axios

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,14 +39,24 @@ const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: [],
+      defaultSrc: ["'self'", 'data:', 'blob:'],
+      baseUri: ["'self'"],
       connectSrc: ["'self'", ...connectSrcUrls],
-      scriptSrc: ["'self'", ...scriptSrcUrls],
-      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-      workerSrc: ["'self'", 'blob:'],
-      objectSrc: [],
+      scriptSrc: [
+        "'self'",
+        'http:',
+        'https://*.cloudflare.com',
+        'https://*.stripe.com',
+        ...scriptSrcUrls,
+      ],
+      frameSrc: ["'self'", 'https://*.stripe.com'],
+      styleSrc: ["'self'", 'https:', "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'data:', 'blob:'],
+      objectSrc: ["'none'"],
+      childSrc: ["'self'", 'blob:'],
       imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
-      fontSrc: ["'self'", ...fontSrcUrls],
+      fontSrc: ["'self'", 'https:', 'data', ...fontSrcUrls],
+      upgradeInsecureRequests: [],
     },
   })
 );
@@ -63,7 +74,9 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json({ limit: '10kb' })); //express.json (middleware)
+//Body parser - lendo dados do body em req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
