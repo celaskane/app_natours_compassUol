b45eefdef7569a438740428b63d298dfec4cea83
Leandro Assis <leandrou9@gmail.com>
2023-01-20 19:22:37 -0300

    Inluindo protect ao router tours

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tours/:slug', viewController.getTour);
+router.get('/tours/:slug', authController.protect, viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
